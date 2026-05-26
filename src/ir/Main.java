d6f69b708fbfd6770c968525d7d219b71f954e6b
Tobias310504 <tobias310504@gmail.com>
2026-05-26 17:46:20 +0700

    first commit

--- src/ir/Main.java
+++ src/ir/Main.java
@@ -6,9 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Membuat object IRApplication.
-        IRApplication search = new IRAppilcation();
         // Method ini hanya menjadi titik masuk program.
-        search.run();
         // Semua alur utama dijalankan di IRApplication.
     }
 }
